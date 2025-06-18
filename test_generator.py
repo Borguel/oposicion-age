bb826db953b7352697d6b30b19691b7717c58ab4
Borguel <borjamiceli@gmail.com>
2025-06-18 17:49:40 +0200

    Forzar redeploy modificando test_generator.py

--- test_generator.py
+++ test_generator.py
@@ -88,3 +88,4 @@ def generar_simulacro(db, num_preguntas=30):
     temas = [doc.id for doc in db.collection("temario").stream()]
     temas_seleccionados = random.sample(temas, min(len(temas), 5))
     return generar_test_avanzado(temas_seleccionados, db, num_preguntas)
+# cambio forzado para despliegue
